b875e50c70c36935fbcb33ad0e34c6204d0eeb69
scanning <scanning@users.noreply.github.com>
2021-04-11 23:27:04 +0000

    deployed via Gatsby Publish Action 🎩 for 5f2a7bfec4b3b4f38c15d9affe60e3e1b50eb989

---
+++ component---src-pages-404-js-7c344574d86c5e2c0d44.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(n,e,t){"use strict";t.r(e);var a=t("q1tI"),u=t.n(a);e.default=function(){return u.a.createElement("div",null,u.a.createElement("h1",null,"404 Page Not Found"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-7c344574d86c5e2c0d44.js.map
